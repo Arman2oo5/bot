ad78801e4a8ef826694dd60297fe6cff25c0bf00
unknown <service.tek.bot@mail.ru>
2026-06-01 15:39:27 +0500

    Diploy

--- connector.py
+++ connector.py
@@ -7,8 +7,8 @@ import psycopg2
 def get_connection():
     try:
         #conn = sqlite3.connect('db.sqlite3')
-        conn = sqlite3.connect('C://django//2025-2026//shop.bot//db.sqlite3')
-        # conn = psycopg2.connect("postgres://shop_admin:in6E36jBWmzdsLPgCzNFjjaXDJmWCCtd@dpg-cogjk3sf7o1s7380l2j0-a.frankfurt-postgres.render.com/shop_bot_pez3", sslmode="require")
+        # conn = sqlite3.connect('C://django//2025-2026//shop.bot//db.sqlite3')
+        conn = psycopg2.connect("postgresql://shop_ifsl_user:BXAfX6IzDrACQPvHN63YJCWdL3CS16ke@dpg-d8elp1c2m8qs7392gdb0-a.frankfurt-postgres.render.com/shop_ifsl", sslmode="require")
         return conn  
     except Exception as error:
         print(error)
